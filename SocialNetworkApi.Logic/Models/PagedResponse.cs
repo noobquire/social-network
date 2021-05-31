5317a466632c71f8048f3216894145e8fe70acd4
Oleksii Lytvynov <Oleksii_Lytvynov@epam.com>
2021-05-31 21:45:26 +0300

    Added pagination models and implementation for users

---
+++ SocialNetworkApi.Logic/Models/PagedResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SocialNetworkApi.Services.Models
+{
+    public class PagedResponse<T>
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public PagedResponse(IEnumerable<T> data, int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.Data = data;
+        }
+    }
+}
